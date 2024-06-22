991c8a4b07429cae032b04661c7fee85b9996082
user <ehalsh2007@naver.com>
2024-06-22 18:22:02 +0900

    responsebod

--- sbb/src/main/java/com/example/sbb/MainController.java
+++ sbb/src/main/java/com/example/sbb/MainController.java
@@ -2,11 +2,15 @@ package com.example.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @RequestMapping("/sbb")
-    public void index() {
-        System.out.println("첫시작");
+    // 아래 함수의 리턴값을 그대로 브라우저에 표시
+    // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
+    @ResponseBody
+    public String  index() {
+        return "안녕하세요";
     }
 }
