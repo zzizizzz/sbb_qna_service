cbddeebacd1d97f07b6de2446d936d422a924f09
user <ehalsh2007@naver.com>
2024-06-22 18:15:14 +0900

    "세팅"

--- sbb/src/main/java/com/example/sbb/MainController.java
+++ sbb/src/main/java/com/example/sbb/MainController.java
@@ -1,2 +1,12 @@
-package com.example.sbb;public class MainController {
+package com.example.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/sbb")
+    public void index() {
+        System.out.println("첫시작");
+    }
 }
